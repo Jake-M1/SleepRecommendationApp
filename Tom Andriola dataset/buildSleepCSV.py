434c21e18c2bca448a432742b426cdfc27244097
TristanHaHa <tristaqh@uci.edu>
2023-02-22 23:39:53 -0800

    populated missing sleep data

--- Tom Andriola dataset/buildSleepCSV.py
+++ Tom Andriola dataset/buildSleepCSV.py
@@ -1,28 +1,43 @@
 import pandas as pd
 
-def buildCaffeineDf(hour=18):
+def buildNutDf():
     # read csvs
     nutCsvs = ["Andriola daily servings summary 200101 thru 201231.csv","Andriola daily servings summary 210101 thru 211231.csv","Andriola daily servings summary 220101 thru 220615.csv"]
-    cafDf = pd.concat( [pd.read_csv(csv) for csv in nutCsvs], ignore_index=True)
+    nutCsv = pd.concat( [pd.read_csv(csv) for csv in nutCsvs], ignore_index=True)
+
+    # fill in missing time values with average meal time
+    avgMealTime = nutCsv.groupby('Group')['Time'].apply(lambda x: pd.to_datetime(x).dropna().mean())
+    nutCsv['Time'] = pd.to_datetime(nutCsv['Time'])
+    for meal in nutCsv['Group'].unique():
+        nutCsv.loc[(nutCsv['Group'] == meal) & (nutCsv['Time'].isna()), 'Time'] = avgMealTime[meal]
+    nutCsv['Time'] = nutCsv['Time'].dt.strftime('%H:%M')
 
     # create datetime column
-    cafDf['Datetime'] = pd.to_datetime(cafDf['Day'] + ' ' + cafDf['Time'])
+    nutCsv['Datetime'] = pd.to_datetime(nutCsv['Day'] + ' ' + nutCsv['Time'])
+
+    # summarize food stats
+    columns = []
+    categories = ['Alcohol (g)', 'Water (g)','Sugars (g)', 'Caffeine (mg)']
+    for c in categories:
+        columns.append(nutCsv.groupby(pd.Grouper(key='Datetime', freq='D'))[c].sum())
 
     # calculate caffeine intake before/after 6:00pm
-    cafAfter6 = cafDf[cafDf['Datetime'].dt.hour >= hour].groupby(pd.Grouper(key='Datetime', freq='D'))["Caffeine (mg)"].sum().rename('Caffeine After 6 (mg)')
-    cafBefore6 = cafDf[cafDf['Datetime'].dt.hour < hour].groupby(pd.Grouper(key='Datetime', freq='D'))["Caffeine (mg)"].sum().rename('Caffeine Before 6 (mg)')
+    hour=18
+    cafBefore6 = nutCsv[nutCsv['Datetime'].dt.hour < hour].groupby(pd.Grouper(key='Datetime', freq='D'))["Caffeine (mg)"].sum().rename('Caffeine Before 6 (mg)')
+    cafAfter6 = nutCsv[nutCsv['Datetime'].dt.hour >= hour].groupby(pd.Grouper(key='Datetime', freq='D'))["Caffeine (mg)"].sum().rename('Caffeine After 6 (mg)')
+    columns.append(cafBefore6)
+    columns.append(cafAfter6)
 
     # return dataframe
-    return pd.concat([cafBefore6, cafAfter6], axis=1)
+    return pd.concat(columns, axis=1)
 
 def buildDf():
     # read csvs
     sleepDf = pd.read_csv("oura_2020-04-14_2022-06-11_trends.csv",index_col=[0], parse_dates=[0])
-    nutDf = pd.read_csv("Nutrition Summary Jan 2019 thru July 2021 from Cronometer.csv",index_col=[0], parse_dates=[0])
-    cafDf = buildCaffeineDf()
+    nutDf = buildNutDf()
 
-    # merge sleep csv with nutrition and 
-    df = pd.concat([sleepDf, nutDf, cafDf], axis=1, join='outer')
+    # merge sleep csv with nutrition data
+    df = pd.concat([sleepDf, nutDf], axis=1, join='outer')
     df['Date'] = df.index
 
     # filter relevant columns
@@ -33,7 +48,7 @@ def buildDf():
     return df
 
 def main():
-    # write csv
+    # get merged data and write to csv
     df = buildDf()
     df.to_csv("sleep.csv", index=False)
 
