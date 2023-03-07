ee5b2efcbd18707470e34eab08f210fb684db4c3
kalmanwong <64678874+kalmanwong@users.noreply.github.com>
2023-03-06 18:18:10 -0800

    Update tab3.page.ts
    - need to access row in sleep database
    - need to access personal model
    - need to compare percent difference for top 5 recommendations

--- snoo-z/src/app/pages/tab3/tab3.page.ts
+++ snoo-z/src/app/pages/tab3/tab3.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { PersonalModelData } from 'src/app/data/personalModel-data';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { UserData } from 'src/app/data/user-data';
 
 
 @Component({
@@ -12,6 +13,22 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 export class Tab3Page implements OnInit{
   recs = ["This is dummy data", "Sleep Earlier", "Sleep Later", "Stop caffeine intake"];
   showing: boolean = true;
+  recentDate = new Date(2000, 1, 1);
+  date = new Date();
+
+  recentModel = {
+    'total_sleep_duration': 0, 
+    'awake_time': 1404.85, 
+    'bedtime_start': '', 
+    'bedtime_end': '', 
+    'steps': 0, 
+    'alcohol': 0, 
+    'water': 0, 
+    'sugar': 0, 
+    'caffeine': 0, 
+    'caffeine_before': 0, 
+    'caffeine_after': 0
+  }
 
   personalModel = {
     'total_sleep_duration': 28918.86, 
@@ -28,6 +45,8 @@ export class Tab3Page implements OnInit{
   }
 
   private model:PersonalModelData = new PersonalModelData(this.personalModel);
+  //private model:PersonalModelData[] | undefined;
+  private user:UserData[] | undefined;
 
   percentDifference(oldValue: number, newValue: number): number {
     const difference = Math.abs(newValue - oldValue);
@@ -35,11 +54,16 @@ export class Tab3Page implements OnInit{
     return percent;
   }
 
+  createDate(date:String){
+   const dateArr = date.split("-");
+   return new Date(Number(dateArr[0]), Number(dateArr[1]), Number(dateArr[2]));
+  }
+
   constructor(private firestore: AngularFirestore, private authService: AuthenticationService) {}
   ngOnInit(): void {
-    console.log(this.model.awake_time);
+    //console.log(this.model.awake_time);
 
-    // this.firestore.collection('/user').snapshotChanges().subscribe(res =>{
+    // this.firestore.collection('/model').snapshotChanges().subscribe(res =>{
     //   this.model = res.map((e:any) =>{
     //     return new PersonalModelData(e.payload.doc.data());
     //   })
@@ -49,6 +73,48 @@ export class Tab3Page implements OnInit{
     // })
 
     // console.log(this.model);
+
+    
+    this.firestore.collection('/user').snapshotChanges().subscribe(res =>{
+      this.user = res.map((e:any) =>{
+           return new UserData(e.payload.doc.data());
+           
+      })
+      this.user?.forEach((data) =>{
+        this.date = this.createDate(data['date']);
+        if(this.recentDate == null){
+          this.recentDate = this.date;
+          this.recentModel['total_sleep_duration'] = data['total_sleep_duration'];
+          this.recentModel['awake_time'] = data['awake_time'];
+          this.recentModel['bedtime_start'] = data['bedtime_start'];
+          this.recentModel['bedtime_end'] = data['bedtime_end'];
+          this.recentModel['steps'] = data['steps'];
+          // this.recentModel['alcohol'] = data['alcohol'];
+          // this.recentModel['water'] = data['water'];
+          // this.recentModel['sugar'] = data['sugar'];
+          // this.recentModel['caffeine'] = data['caffeine'];
+          // this.recentModel['caffeine_before'] = data['caffeine_before'];
+          // this.recentModel['caffeine_after'] = data['caffeine_after'];
+        }else if(this.date > this.recentDate){
+          this.recentDate = this.date;
+          this.recentModel['total_sleep_duration'] = data['total_sleep_duration'];
+          this.recentModel['awake_time'] = data['awake_time'];
+          this.recentModel['bedtime_start'] = data['bedtime_start'];
+          this.recentModel['bedtime_end'] = data['bedtime_end'];
+          this.recentModel['steps'] = data['steps'];
+          // this.recentModel['alcohol'] = data['alcohol'];
+          // this.recentModel['water'] = data['water'];
+          // this.recentModel['sugar'] = data['sugar'];
+          // this.recentModel['caffeine'] = data['caffeine'];
+          // this.recentModel['caffeine_before'] = data['caffeine_before'];
+          // this.recentModel['caffeine_after'] = data['caffeine_after'];
+        }
+      });
+      console.log(this.recentDate);
+    });
   }
 
+
+
+  
 }
